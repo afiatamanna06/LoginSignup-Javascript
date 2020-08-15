77c5272c1e989c1a94a47cb6dafa3956a08ab0d8
afiatamanna06 <afiatamanna06@gmail.com>
2020-08-15 19:03:11 +0600

    index.html updated

---
+++ index.js
@@ -0,0 +1,15 @@
+<
+script >
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyC2ec7KFkjgMQSlMp467GxS7VfAr3vbSNA",
+        authDomain: "form-d47d9.firebaseapp.com",
+        databaseURL: "https://form-d47d9.firebaseio.com",
+        projectId: "form-d47d9",
+        storageBucket: "form-d47d9.appspot.com",
+        messagingSenderId: "123339361861",
+        appId: "1:123339361861:web:49f7d479d11b4606299d2c"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); <
+/script>
