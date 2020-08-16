df680511a7160c6ce216ff456d246cdde43e5001
afiatamanna06 <afiatamanna06@gmail.com>
2020-08-16 19:27:18 +0600

    index.js updated

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function signUp() {
     signup.catch(e => alert(e.message));
     alert("Signed Up");
 }
+
+function signIn() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    const signup = auth.signInWithEmailAndPassword(email.value, password.value);
+    signup.catch(e => alert(e.message));
+    alert("Signed In" + email);
+}
