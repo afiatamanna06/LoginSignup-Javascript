5e268d5117343bf0c3656c6e2d7cbd8cfb7f3bb9
afiatamanna06 <afiatamanna06@gmail.com>
2020-08-16 19:21:11 +0600

    index.js updated

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-<
-script >
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyC2ec7KFkjgMQSlMp467GxS7VfAr3vbSNA",
-        authDomain: "form-d47d9.firebaseapp.com",
-        databaseURL: "https://form-d47d9.firebaseio.com",
-        projectId: "form-d47d9",
-        storageBucket: "form-d47d9.appspot.com",
-        messagingSenderId: "123339361861",
-        appId: "1:123339361861:web:49f7d479d11b4606299d2c"
-    };
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC2ec7KFkjgMQSlMp467GxS7VfAr3vbSNA",
+    authDomain: "form-d47d9.firebaseapp.com",
+    databaseURL: "https://form-d47d9.firebaseio.com",
+    projectId: "form-d47d9",
+    storageBucket: "form-d47d9.appspot.com",
+    messagingSenderId: "123339361861",
+    appId: "1:123339361861:web:49f7d479d11b4606299d2c"
+};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig); <
-/script>
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+function signUp() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    const signup = auth.createUserWithEmailAndPassword(email.value, password.value);
+    signup.catch(e => alert(e.message));
+    alert("Signed Up");
+}
